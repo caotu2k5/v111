15d48eed5871c89b18d6ae2f2123a473ede507f2
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-08-14 22:22:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v111/raw/main/v111.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x sigint1 && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x tmwl
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v111/raw/main/v111.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/t1 && mv t1 motd
 clear
